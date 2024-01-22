bc78f99053bb507580df7436b663b318820aa9ad
Crusiris <nunezcrusiris@gmail.com>
2024-01-21 23:24:40 -0300

    adding condition to the ui

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -5,16 +5,18 @@ import { GameContext } from '../../context/GameContextProvider';
 const Board = () => {
     const { cards }=useContext(GameContext);
     return ( 
-        <section class="container mx-auto px-[12px] md:px-24 xl:px-12 max-w-[1300px] py-10">
-            <div className='capitalize pb-12 flex flex-col justify-center items-center gap-y-2'>
-                 <h1 class="text-center text-5xl">memory game</h1>
-                 <p className='text-xl lowercase'>Test your memory with this exciting game</p> 
+        <section className='bg-gradient-to-t from-purple-100'>
+            <div class="container mx-auto px-[12px] md:px-24 xl:px-12 max-w-[1300px] py-10 h-screen ">
+                <div className='capitalize pb-12 flex flex-col justify-center items-center gap-y-2'>
+                    <h1 class="text-center text-5xl text-purple-500">memory game</h1>
+                    <p className='text-xl lowercase text-center text-purple-400'>Test your memory with this exciting game</p> 
+                </div>
+                <ul  class="grid grid-cols-3 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-8 justify-items-center gap-2 lg:gap-3">
+                    {cards.map((card) => (
+                    <Card key={card.uuid} card={card} />
+                    ))} 
+                </ul> 
             </div>
-            <ul  class="grid grid-cols-3 xl:grid-cols-4 gap-2 lg:gap-3">
-                {cards.map((card) => (
-                <Card key={card.uuid} card={card} />
-                ))} 
-            </ul> 
         </section>
         
      );
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
+import { useContext } from 'react'
+import { GameContext } from '../../context/GameContextProvider'
 
 const Card = ({card}) => {
-  const { title, url } = card
+  const { title, url,uuid, flippe} = card
+const {flipCards} = useContext(GameContext)
+
+  const handleCard = (id)=>{
+    console.log(id)
+    flipCards(id)
+  }
+   
   return (
-    <li class="relative group h-24 lg:h-48     flex flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md overflow-hidden cursor-pointer"> 
-        <img className='object-cover bg-top h-full w-full ' src={url} alt={title} />
-    </li>
-  )
+    
+<div className={`drop-shadow-md flex items-center ${flippe ? '[transform: rotateY(10deg)]' : 'bg-white'} justify-center cursor-pointer h-24 w-24 lg:h-32 lg:w-32 overflow-hidden hover:scale-105 rounded-xl transition-all duration-1000`}
+    onClick={()=>handleCard(uuid)}>
+    <div>
+        <img src={url} alt={title}
+            className={`h-24 w-24 lg:h-32 lg:w-32 scale-110 object-cover ${!flippe
+                    ? '[transform:rotateY(180deg)] [backface-visibility:hidden] transition-all duration-1000'
+                    : ''
+            }`}
+        />
+    </div>
+</div>)
 }
 
 export default Card
