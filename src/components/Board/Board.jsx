be28aa8c637165bc30f2582400b1d22528ecbc39
Crusiris <nunezcrusiris@gmail.com>
2024-01-24 18:04:25 -0300

    fix styles

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -11,23 +11,20 @@ const Board = () => {
     }
 
     return ( 
-        <section className='h-full'>
+        <section className='h-screen'>
             <div className="h-full container mx-auto px-[12px] md:px-24 xl:px-12 max-w-[1300px] py-10">
                 <div className='capitalize pb-12 flex flex-col justify-center items-center gap-y-2'>
                     <h1 className="text-center text-5xl text-purple-500">memory game</h1>
                     <p className='text-xl lowercase text-center text-purple-400'>Test your memory with this exciting game</p> 
-                    {cards.length > 0 ? 
-                        (
-                            <div className='flex items-center justify-center mt-12'>
-                            <button onClick={handleNewGame} type="button" className="inline-flex w-full justify-center rounded-md bg-purple-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-purple-600 sm:ml-3 sm:w-auto">New game</button>
-                            </div>
-                        ):null
-                    }
-                   
+                        <div className='flex items-center justify-center mt-12'>
+                            <button onClick={handleNewGame} type="button" 
+                            className={`${cards.length > 0 ? ' hover:bg-purple-600 sm:ml-3 sm:w-auto' : 'pointer-events-none opacity-40'} bg-purple-500 px-3 py-2 text-sm font-semibold text-white shadow-sm inline-flex w-full justify-center rounded-md `}
+                            >New game</button>
+                        </div> 
                 </div>
                 
                 {cards.length>0 ? (
-                    <ul  className="grid grid-cols-3 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-8 justify-items-center gap-2 lg:gap-3">
+                    <ul  className="h-full grid grid-cols-3 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-8 justify-items-center gap-2 lg:gap-3">
                         {cards.map((card) => (
                         <Card key={card.uuid} card={card} />
                         ))} 
